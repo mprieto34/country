c9c1d3b4ac8a47887312f365bd68a014d71f5c59
Matthias <matthias.prieto@redsalud.gob.cl>
2024-01-26 10:23:01 -0300

    actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { SharedModule } from "./shared/shared.module";
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { HttpHandler } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+    declarations: [
+        AppComponent,
+    ],
+
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      SharedModule,
+      RouterModule,
+      HttpClientModule
+
   ],
   providers: [
     provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+],
+    bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-by-capital-page',
+  templateUrl: './by-country-page.component.html',
+  styles: [
+
+  ]
+})
+export class ByCountryPageComponent {
+
+  public countries: Country[] = [];
+  constructor ( private countriesService: CountriesService ){}
+
+  searchByCountry ( term: string ): void {
+    this.countriesService.searchCountry ( term )
+      .subscribe( countries => {
+          this.countries = countries;
+      } );
+  }
+
+}
